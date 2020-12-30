7af53e27c06ba418707a25e26fcd97bc7bef607a
Luiz Henrique <luizh11tmj@gmail.com>
2020-12-30 02:57:05 -0300

    upload web archives

---
+++ src/web/schema/index.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = require("./Anime")
+
+//mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost/animes",{useNewUrlParser: true, useUnifiedTopology: true})
+
+// .env com a string de conexão com o mongo
+
+
+mongoose.model("animes", Schema);
+
+const model = mongoose.model("animes");
+module.exports = model;
